d155a967bf75177db235255345f20f19abd8e2ac
Quentin <quentin.mettier@edu.ecole-89.com>
2024-01-09 10:51:35 +0100

    again

--- src/translate.c
+++ src/translate.c
@@ -4,16 +4,25 @@
 int translate(char *to_replace, char *substitutes)
 {
     char *buffer;
-    char *new_text;
     int count;
+    char *new_text;
+    int letter;
 
     count = 0;
+    letter = 0;
     if (stu_strlen(to_replace) != stu_strlen(substitutes))
-        return 5;
-    buffer = buffer();
-    new_text = malloc(sizeof(char) * stu_strlen(buffer) + 1);
+        return 1;
     while (count < stu_strlen(buffer)) {
-        count = core(count);
+        while (letter < stu_strlen(to_replace)) {
+            if (buffer[count] == to_replace[letter]) {
+                new_text[count] = substitutes[letter];
+                letter = 0;
+                count += 1;
+            }
+            letter += 1;
+        }
+        new_text[count] = buffer[count];
+        count += 1;
     }
-    return 0;
+    write(1, new_text, 10);
 }
