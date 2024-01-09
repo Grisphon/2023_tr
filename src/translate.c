f672b6bb6829a8fed0a2de67410e0cb7f436f61e
Quentin <quentin.mettier@edu.ecole-89.com>
2024-01-09 10:30:37 +0100

    e

--- src/translate.c
+++ src/translate.c
@@ -1,12 +1,30 @@
-void translate(char *to_replace, char *substitutes)
+#include <stdlib.h>
+#include "stutr.h"
+
+int translate(char *to_replace, char *substitutes)
 {
     char *buffer;
-    int size_read;
+    char *new_text;
+    int count;
+    int letter;
 
+    count = 0;
+    letter = 0;
     if (stu_strlen(to_replace) != stu_strlen(substitutes))
         return 5;
-    size_read = read(1, buffer, 10);
-    while (size_read != 0) {
-        size_read = read(1, buffer, 10);
+    buffer = buffer();
+    new_text = malloc(sizeof(char) * stu_strlen(buffer) + 1);
+    while (count < stu_strlen(buffer)) {
+        while (letter < stu_strlen(to_replace)) {
+            if (buffer[count] == to_replace[letter]) {
+                new_text[count] = substitutes[letter];
+                letter = 0;
+                count += 1;
+            }
+            letter += 1;
+        }
+        new_text[count] = buffer[count];
+        count += 1;
     }
+    return 0;
 }
